ce814931441d4309a518af1e23f2eeec785429ed
karasu <GTR12534@Gmail.com>
2020-06-18 00:16:25 +0800

    available to show title, author, and snippet

--- crawler.py
+++ crawler.py
@@ -4,14 +4,13 @@ from bs4 import BeautifulSoup
 if __name__ == "__main__":
     keyword = input("Enter your keyword: ")
     
-    # a class="labs-docsum-title" 
-    # div class="labs-docsum-citation full-citation"
-    # div class="labs-docsum-snippet"
     link = "https://pubmed.ncbi.nlm.nih.gov/?term=" + keyword
     web = requests.get(link)
+
     soup = BeautifulSoup(web.text, "html.parser")
     title = soup.select("a.labs-docsum-title")
-    author = soup.select("div.labs-docsum-citation full-citation")
-    snippet = soup.select("div.labs-docsum-snippet")
-    for t in title:
-        print(t.text.strip())
+    author = soup.select("span.labs-docsum-authors")
+    snippet = soup.select("div.full-view-snippet")
+
+    for t, a, s in zip(title, author, snippet):
+        print(t.text.strip(), a.text.strip(), s.text.strip(), sep='\n', end='\n\n')
